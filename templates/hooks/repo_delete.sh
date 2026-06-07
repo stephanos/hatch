c07fe681e09a7a17bfe6f8ba86a0cd291393b197
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-07 16:31:01 -0700

    run git inside dir

--- templates/hooks/repo_delete.sh
+++ templates/hooks/repo_delete.sh
@@ -8,9 +8,7 @@
 repo_path="$(hatch_arg_value --required --repo-path "$@")"
 
 # Resolve the local branch.
-# Move into the repo to inspect the current branch.
-cd "$repo_path"
-branch="$(git branch --show-current)"
+branch="$(git -C "$repo_path" branch --show-current)"
 if [ -z "$branch" ]; then
   exit 0
 fi
@@ -24,7 +22,7 @@ fi
 
 # Delete branch and checkout.
 # Delete remote branch (if present) and remove local repo directory.
-if git show-ref --verify --quiet "refs/remotes/origin/$branch"; then
-  git push --delete origin "$branch" || exit 0
+if git -C "$repo_path" show-ref --verify --quiet "refs/remotes/origin/$branch"; then
+  git -C "$repo_path" push --delete origin "$branch" || exit 0
 fi
 rm -rf "$repo_path"
